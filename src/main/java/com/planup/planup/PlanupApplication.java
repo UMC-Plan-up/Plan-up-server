d41dba2539e7389d28192b3168a07469ee88e6ff
leesuyong849 <toosee1005@cau.ac.kr>
2025-07-05 22:23:45 +0900

    init

---
+++ src/main/java/com/planup/planup/PlanupApplication.java
@@ -0,0 +1,14 @@
+package com.planup.planup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class PlanupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/planup/planup/PlanupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.planup.planup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
