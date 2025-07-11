16ae429aa1249f74743eb556c1f33a451a1e118a
leesuyong849 <toosee1005@cau.ac.kr>
2025-07-11 16:25:45 +0900

    FEAT: user과 관련된 필드에 대한 간단한 설정

---
+++ src/main/java/com/planup/planup/domain/user/entity/User.java
@@ -0,0 +1,53 @@
+package com.planup.planup.domain.user.entity;
+
+import com.planup.planup.domain.friend.entity.Friend;
+import com.planup.planup.domain.global.entity.BaseTimeEntity;
+import com.planup.planup.domain.oauth.entity.OAuthAccount;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String password;
+    private String nickname;
+
+    @Enumerated(EnumType.STRING)
+    private UserActivate userActivate;
+
+    @Enumerated(EnumType.STRING)
+    private UserLevel userLevel;
+
+    @Lob
+    private String profileImg;
+
+    private String socialEmail;
+    private Boolean alarmAllow;
+    private String inviteCode;
+
+
+    // 연관 관계
+    @OneToMany(mappedBy = "user")
+    private List<UserTerms> userTermList;
+
+    @OneToMany(mappedBy = "user")
+    private List<InvitedUser> invitedUserList;
+
+    @OneToMany(mappedBy = "user")
+    private List<OAuthAccount> oAuthAccountList;
+
+    @OneToMany(mappedBy = "user")
+    private List<Friend> friendList;
+}
