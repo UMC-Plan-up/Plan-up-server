f0a5f95d2443bb456782649ca33f5a120712cc07
leesuyong849 <toosee1005@cau.ac.kr>
2025-07-17 17:00:16 +0900

    ad

--- src/main/java/com/planup/planup/config/SecurityConfig.java
+++ src/main/java/com/planup/planup/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.planup.planup.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -13,19 +14,19 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf().disable()
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
-                                "/", // 홈도 열어두고 싶다면 추가
-                                "/api/v1/users/login", "/api/v1/users", "/api/v1/users/duplicate", "/api/v1/users/check-nickname",
-                                "/swagger-ui/**", "/api-docs/**", "/api/v1/oauth2/**", "/login/oauth2/**", "/oauth2/**"
+                                "/",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/v3/api-docs/**",
+                                "/swagger-resources/**",
+                                "/webjars/**"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
-                .formLogin(form -> form
-                        .loginPage("/login") // 커스텀 로그인 페이지를 쓰지 않으면 생략해도 됨
-                        .permitAll()
-                );
+                .formLogin(Customizer.withDefaults());
 
         return http.build();
     }
