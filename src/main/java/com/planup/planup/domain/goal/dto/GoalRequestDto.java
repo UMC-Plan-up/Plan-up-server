5addcdc88b06e82bd95c367e11b26b0914493b94
woojoo48 <wjddnwn0408@gmail.com>
2025-07-22 13:26:43 +0900

    [Feat] : 사용자 목표 조회 API DTO 추가
    goal을 가져오면 되는 세부 목표 조회와 달리 usergoal 엔터티와 goal엔터티의
    특정 필드들을 요구하기에 DTO 개발
    
    IssueNum: #23

--- src/main/java/com/planup/planup/domain/goal/dto/GoalRequestDto.java
+++ src/main/java/com/planup/planup/domain/goal/dto/GoalRequestDto.java
@@ -1,6 +1,7 @@
 package com.planup.planup.domain.goal.dto;
 
 import com.planup.planup.domain.goal.entity.Enum.GoalCategory;
+import com.planup.planup.domain.goal.entity.Enum.GoalPeriod;
 import com.planup.planup.domain.goal.entity.Enum.GoalType;
 import lombok.Data;
 import lombok.Getter;
@@ -17,9 +18,10 @@ public class GoalRequestDto {
         GoalCategory goalCategory;
         GoalType goalType;
         String oneDose;
+        int frequency;
+        GoalPeriod period;
         Date endDate;
         Boolean isChallenge;
-        String currentAmount;
         int limitFriendCount;
     }
 }
--- src/main/java/com/planup/planup/domain/goal/dto/GoalResponseDto.java
+++ src/main/java/com/planup/planup/domain/goal/dto/GoalResponseDto.java
@@ -1,9 +1,9 @@
 package com.planup.planup.domain.goal.dto;
 
-import com.planup.planup.domain.goal.entity.Enum.GoalCategory;
-import com.planup.planup.domain.goal.entity.Enum.GoalType;
+import com.planup.planup.domain.goal.entity.Enum.*;
 import lombok.*;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
 
@@ -20,9 +20,54 @@ public class GoalResponseDto {
         GoalCategory goalCategory;
         GoalType goalType;
         String oneDose;
+        int frequency;
+        GoalPeriod period;
+        Date endDate;
+        Boolean isChallenge;
+        int limitFriendCount;
+    }
+
+    //목표 조회 리스트 Dto
+    @Builder
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class MyGoalListDto {
+        Long goalId;
+        String goalName;
+        GoalCategory goalCategory;
+        GoalType goalType;
+        VerificationType verificationType;
+        int frequency;
+        String oneDose;
+        String currentAmount;
+        String creatorNickname;
+        String creatorProfileImg;
+        Status myStatus;
+        int participantCount;
+        Boolean isActive;
+    }
+
+    //목표 세부 조회 Dto
+    @Builder
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class MyGoalDto {
+        Long goalId;
+        String goalName;
+        String goalAmount;
+        GoalCategory goalCategory;
+        GoalType goalType;
+        String oneDose;
         Date endDate;
         Boolean isChallenge;
         String currentAmount;
         int limitFriendCount;
+        Status myStatus;
+        Boolean isActive;
+        VerificationType verificationType;
+        int participantCount;
+        LocalDateTime createdAt;
     }
 }
