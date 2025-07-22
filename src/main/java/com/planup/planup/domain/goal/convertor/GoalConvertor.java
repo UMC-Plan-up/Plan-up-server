a68f76d5f80aa5cb30e35d5e70179d37f7e16808
woojoo48 <wjddnwn0408@gmail.com>
2025-07-22 13:28:47 +0900

    [Feat] : 사용자 목표 조회 API 변환 메서드 개발
    목표 하나에 대한 DTO 변환 후 계산된 결과 값을 담아 List에 담아 반환
    
    IssueNum: #23

--- src/main/java/com/planup/planup/domain/goal/convertor/GoalConvertor.java
+++ src/main/java/com/planup/planup/domain/goal/convertor/GoalConvertor.java
@@ -3,6 +3,12 @@ package com.planup.planup.domain.goal.convertor;
 import com.planup.planup.domain.goal.dto.GoalRequestDto;
 import com.planup.planup.domain.goal.dto.GoalResponseDto;
 import com.planup.planup.domain.goal.entity.Goal;
+import com.planup.planup.domain.goal.entity.mapping.UserGoal;
+import com.planup.planup.domain.user.entity.User;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class GoalConvertor {
 
@@ -15,6 +21,8 @@ public class GoalConvertor {
                 .goalType(createGoalDto.getGoalType())
                 .oneDose(createGoalDto.getOneDose())
                 .endDate(createGoalDto.getEndDate())
+                .period(createGoalDto.getPeriod())
+                .frequency(createGoalDto.getFrequency())
                 .isChallenge(createGoalDto.getIsChallenge())
                 .limitFriendCount(createGoalDto.getLimitFriendCount())
                 .build();
@@ -28,9 +36,51 @@ public class GoalConvertor {
                 .goalCategory(goal.getGoalCategory())
                 .goalType(goal.getGoalType())
                 .oneDose(goal.getOneDose())
+                .period(goal.getPeriod())
+                .frequency(goal.getFrequency())
                 .endDate(goal.getEndDate())
                 .isChallenge(goal.getIsChallenge())
                 .limitFriendCount(goal.getLimitFriendCount())
                 .build();
+    }
+
+    //내 목표 리스트 조회 변환(DTO 가져오기)
+    public static GoalResponseDto.MyGoalListDto toMyGoalListDto(
+            UserGoal userGoal,
+            User creator,
+            int participantCount) {
+
+        Goal goal = userGoal.getGoal();
+
+        return GoalResponseDto.MyGoalListDto.builder()
+                .goalId(goal.getId())
+                .goalName(goal.getGoalName())
+                .goalCategory(goal.getGoalCategory())
+                .goalType(goal.getGoalType())
+                .verificationType(userGoal.getVerificationType())
+                .frequency(goal.getFrequency())
+                .oneDose(goal.getOneDose())
+                .currentAmount(goal.getCurrentAmount())
+                .creatorNickname(creator.getNickname())
+                .creatorProfileImg(creator.getProfileImg())
+                .myStatus(userGoal.getStatus())
+                .participantCount(participantCount)
+                .isActive(userGoal.getIsActive())
+                .build();
+    }
+
+    //내 목표 리스트 조회 변환(DTO -> List 변환)
+    public static List<GoalResponseDto.MyGoalListDto> toMyGoalListDtoList(
+            List<UserGoal> userGoals,
+            List<User> creators,
+            List<Integer> participantCounts) {
+
+        return IntStream.range(0, userGoals.size())
+                .mapToObj(i -> toMyGoalListDto(
+                        userGoals.get(i),
+                        creators.get(i),
+                        participantCounts.get(i)
+                ))
+                .collect(Collectors.toList());
     }
 }
