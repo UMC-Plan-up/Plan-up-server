3aef8388984cdea0e6717b776c1f400dffb25b4c
woojoo48 <wjddnwn0408@gmail.com>
2025-07-22 13:29:58 +0900

    [Feat] : 사용자 목표 조회 API 쿼리 서비스 추가
    IssueNum: #23

--- src/main/java/com/planup/planup/domain/goal/repository/UserGoalRepository.java
+++ src/main/java/com/planup/planup/domain/goal/repository/UserGoalRepository.java
@@ -1,10 +1,22 @@
 package com.planup.planup.domain.goal.repository;
 
+import com.planup.planup.domain.goal.entity.Enum.Status;
 import com.planup.planup.domain.goal.entity.mapping.UserGoal;
+import org.springframework.boot.autoconfigure.flyway.FlywayProperties;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserGoalRepository extends JpaRepository<UserGoal, Long> {
+    //Query Service
+    List<UserGoal> findByUserIdAndIsActiveTrue(Long userId);
+
+    UserGoal findByGoalIdAndStatus(Long goalId, Status status);
+
+    int countByGoalIdAndIsActiveTrue(Long goalId);
+
+    //Command Service
 
 }
