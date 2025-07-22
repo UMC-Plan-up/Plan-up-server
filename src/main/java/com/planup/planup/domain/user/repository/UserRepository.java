e23b15bba79b6f4494da3fd26cae9460e84e6ecf
woojoo48 <wjddnwn0408@gmail.com>
2025-07-22 13:22:14 +0900

    [Refactor] : findById 중복제거
    IssueNum: #23

--- src/main/java/com/planup/planup/domain/user/repository/UserRepository.java
+++ src/main/java/com/planup/planup/domain/user/repository/UserRepository.java
@@ -8,9 +8,6 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
-    Optional<User> findById(Long id);
-
     boolean existsByNickname(String nickname);
 
     Optional<User> findByEmail(String email);
