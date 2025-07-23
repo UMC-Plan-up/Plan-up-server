ced63edb9b93e7594df1763f41dca7e90d587f60
Kwon-DoHee <152317074+seamooll@users.noreply.github.com>
2025-07-23 16:42:36 +0900

    Feat: 사용자 인증 관련 에러 코드 추가

--- src/main/java/com/planup/planup/apiPayload/code/status/ErrorStatus.java
+++ src/main/java/com/planup/planup/apiPayload/code/status/ErrorStatus.java
@@ -17,10 +17,14 @@ public enum ErrorStatus implements BaseErrorCode {
     _FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403", "금지된 요청입니다."),
 
 
-    //User 에러
+    // User 에러
     NOT_FOUND_USER(HttpStatus.NOT_FOUND, "USER4001", "존재하지 않는 유저입니다"),
-    EXIST_NICKNAME(HttpStatus.CONFLICT, "USER4002", "이미 존재하는 닉네임입니다")
-    ;
+    EXIST_NICKNAME(HttpStatus.CONFLICT, "USER4002", "이미 존재하는 닉네임입니다"),
+    // 로그인, 회원가입 관련 에러
+    USER_EMAIL_ALREADY_EXISTS(HttpStatus.CONFLICT, "USER4003", "이미 존재하는 이메일입니다"),
+    PASSWORD_MISMATCH(HttpStatus.BAD_REQUEST, "USER4004", "비밀번호가 일치하지 않습니다"),
+    INVALID_CREDENTIALS(HttpStatus.UNAUTHORIZED, "USER4005", "이메일 또는 비밀번호가 잘못되었습니다"),
+    USER_INACTIVE(HttpStatus.FORBIDDEN, "USER4006", "비활성화된 계정입니다");
 
     private final HttpStatus httpStatus;
     private final String code;
