374b46fa7e56e06dcb0c8644b0adf20ee472cc72
leesuyong849 <toosee1005@cau.ac.kr>
2025-07-25 18:16:34 +0900

    FEAT: dto와 converter 생성

--- src/main/java/com/planup/planup/domain/notification/dto/NotificationResponseDTO.java
+++ src/main/java/com/planup/planup/domain/notification/dto/NotificationResponseDTO.java
@@ -1,5 +1,16 @@
 package com.planup.planup.domain.notification.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+
 public class NotificationResponseDTO {
 
+    @Builder
+    public record NotificationDTO(
+            @Schema(description = "알림 아이디", example = "1")
+            Long id,
+
+            @Schema(description = "알림 내용", example = "친구 1님의 댓글 '벌써 이만큼 함?'")
+            String notificationText
+    ) {}
 }
