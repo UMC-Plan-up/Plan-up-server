6e9df31b40705e26c0796839378f13c1d3ad4a74
leesuyong849 <toosee1005@cau.ac.kr>
2025-07-26 16:04:10 +0900

    FEAT: challenge용 friend 정보 조회 메서드 완료

--- src/main/java/com/planup/planup/domain/goal/controller/ChallengeController.java
+++ src/main/java/com/planup/planup/domain/goal/controller/ChallengeController.java
@@ -1,16 +1,18 @@
 package com.planup.planup.domain.goal.controller;
 
 import com.planup.planup.apiPayload.ApiResponse;
+import com.planup.planup.domain.friend.dto.FriendResponseDTO;
+import com.planup.planup.domain.friend.service.FriendService;
 import com.planup.planup.domain.goal.dto.ChallengeRequestDTO;
 import com.planup.planup.domain.goal.service.ChallengeService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/challenge")
@@ -18,10 +20,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class ChallengeController {
 
     private final ChallengeService challengeService;
+    private final FriendService friendService;
+
 
     @PostMapping("/create")
+    @Operation(summary = "챌린지 생성 요청", description = "goal을 상속한 challenge를 생성하고 userGoal도 생성한다.")
     public ApiResponse<Void> craeteChallenge (Long userId, @RequestBody @Valid ChallengeRequestDTO.create createDTO) {
         challengeService.createChallenge(userId, createDTO);
         return ApiResponse.onSuccess(null);
     }
+
+    @GetMapping("/friends")
+    public ApiResponse<List<FriendResponseDTO.FriendInfoInChallengeCreate>> getFriendList(Long userId) {
+        List<FriendResponseDTO.FriendInfoInChallengeCreate> frinedListInChallenge = friendService.getFrinedListInChallenge(userId);
+        return ApiResponse.onSuccess(frinedListInChallenge);
+    }
+
 }
