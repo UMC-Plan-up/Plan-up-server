ffbe0eec9d6d4a83d7011364a19e53ecc0fe8e7a
woojoo48 <wjddnwn0408@gmail.com>
2025-07-28 16:35:44 +0900

    [Feat] : 인증 서비스에 팩토리 패턴 적용 세팅
    IssueNum: #38

--- src/main/java/com/planup/planup/domain/goal/service/verification/PhotoVerificationService.java
+++ src/main/java/com/planup/planup/domain/goal/service/verification/PhotoVerificationService.java
@@ -1,8 +1,15 @@
 package com.planup.planup.domain.goal.service.verification;
 
+import com.planup.planup.domain.goal.repository.PhotoVerificationRepository;
+import com.planup.planup.domain.goal.repository.UserGoalRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class PhotoVerificationService implements VerificationService{
+    private final UserGoalRepository userGoalRepository;
+    private final PhotoVerificationRepository photoVerificationRepository;
+
 
 }
