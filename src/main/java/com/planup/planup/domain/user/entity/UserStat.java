59dac1eb36df84b81ca5e809159c577c155ad4cc
leesuyong849 <toosee1005@cau.ac.kr>
2025-07-28 11:27:07 +0900

    FEAT: badge 데이터 처리 메서드 추가

--- src/main/java/com/planup/planup/domain/user/entity/UserStat.java
+++ src/main/java/com/planup/planup/domain/user/entity/UserStat.java
@@ -1,8 +1,10 @@
 package com.planup.planup.domain.user.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
 
 @Entity
+@Getter
 public class UserStat {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -11,28 +13,151 @@ public class UserStat {
     @OneToOne(fetch = FetchType.LAZY)
     User user;
 
-
     /* ========= 하루 기준 ========= */
-    private int friendRequestCnt;           // 친구신청 수
-    private int commentCntInFriendDay;      // 하루에 친구 글에서 댓글 수
-    private int likeCnt;                    // ‘응원해요’
-    private int encourageCnt;               // ‘분발해요’
-    private int goalRecordCnt;              // 목표 기록 수
-    private int pushOpenCnt;                // 푸시 열람 수
-    private int complete3goal;              //하루에 3개 이상의 목표 완료
-    private int requestFriendOneDay;        //하루에 3번 이상 친구 신청
+    private int friendRequestCnt = 0;           // 친구신청 수
+    private int commentCntInFriendDay = 0;      // 하루에 친구 글에서 댓글 수
+    private int likeCnt = 0;                    // ‘응원해요’
+    private int encourageCnt = 0;               // ‘분발해요’
+    private int goalRecordCnt = 0;              // 목표 기록 수
+    private int pushOpenCnt = 0;                // 푸시 열람 수
+    private int complete3goal = 0;              // 하루에 3개 이상의 목표 완료
+    private int requestFriendOneDay = 0;        // 하루에 3번 이상 친구 신청
 
     /* ========= 일주일 기준 ========= */
-    private int reactionCntWeek;            // 전체 반응 버튼
-    private int recordAllGoal7Days;        // 7일 연속 전체 목표 기록
-    private int recordSpecificGoalDays     // 7일 연속 특정 목표 기록
+    private int reactionCntWeek = 0;                        // 전체 반응 버튼
+    private int recordAllGoal7Days = 0;                     // 7일 연속 전체 목표 기록
+    private int recordSpecificGoalDays = 0;                 // 7일 연속 특정 목표 기록
+    private boolean recordAllGoal7DaysFlag = true;          // 7일 연속 전체 목표 기록: 오늘 기록하였는가
+    private boolean recordSpecificGoalDaysFlag = true;      // 7일 연속 특정 목표 기록: 오늘 기록하였는
 
     /* ========= 누적 카운터 ========= */
-    private int profileClickCnt;            // 친구 프로필 클릭
-    private int weeklyStatViewCnt;          // 주간 통계 조회
-    private long totalRecordCnt;            //총 기록 수
-    private long totalInviteShareCnt;       //초대 코드 공유 수
-    private long totalInviteAcceptedCnt;    //초대한 친구 중 가입 수
-    private long totalGoalCreatedCnt;       //목표 생성 수
-    private long totalCommentCnt;           //총 댓글 수
+    private int totalProfileClickCnt = 0;            // 친구 프로필 클릭
+    private int weeklyStatViewCnt = 0;          // 주간 통계 조회
+    private long totalRecordCnt = 0;            // 총 기록 수
+    private long totalInviteShareCnt = 0;       // 초대 코드 공유 수
+    private long totalInviteAcceptedCnt = 0;    // 초대한 친구 중 가입 수
+    private long totalGoalCreatedCnt = 0;       // 목표 생성 수
+    private long totalCommentCnt = 0;           // 총 댓글 수
+
+
+    public void resetDailyStats() {
+        this.friendRequestCnt = 0;
+        this.commentCntInFriendDay = 0;
+        this.likeCnt = 0;
+        this.encourageCnt = 0;
+        this.goalRecordCnt = 0;
+        this.pushOpenCnt = 0;
+        this.complete3goal = 0;
+        this.requestFriendOneDay = 0;
+
+        if (recordAllGoal7DaysFlag) {
+            this.recordAllGoal7Days = 0;
+            this.recordAllGoal7DaysFlag = false;
+        }
+
+        if (recordSpecificGoalDaysFlag) {
+            this.recordSpecificGoalDays = 0;
+            this.recordSpecificGoalDaysFlag = false;
+        }
+    }
+
+    public void resetWeeklyStats() {
+        this.reactionCntWeek = 0;
+    }
+
+    //댓글을 추가한 경우
+    public void addComment() {
+        this.totalCommentCnt += 1;
+    }
+
+    //goal을 추가하는 경우
+    public void addGoal() {
+        this.totalGoalCreatedCnt += 1;
+    }
+
+    //초대코드 코드 공유
+    public void addShareingInviteCode() {
+        this.totalInviteShareCnt += 1;
+    }
+
+    //초대코드 받고 가입하는 경우
+    public void addAcceptInviteCode() {
+        this.totalInviteAcceptedCnt += 1;
+    }
+
+    //인증을 추가한 경우
+    public void addVerfiy() {
+        this.totalRecordCnt += 1;
+    }
+
+    //친구 프로필을 선택한 경우
+    public void addClickFriendProfile() {
+        this.totalProfileClickCnt += 1;
+    }
+
+    //주간 통계 조회 수
+    public void addWeeklyStatViewCnt() {
+        this.weeklyStatViewCnt += 1;
+    }
+
+    //인증을 추가한 경우
+    public void setRecordAllGoal7Days() {
+        if (recordAllGoal7DaysFlag) {
+            return;
+        } else {
+            recordAllGoal7DaysFlag = true;
+            recordAllGoal7Days++;
+        }
+    }
+
+    public void setRecordSpecificGoalDays() {
+        if (recordSpecificGoalDaysFlag) {
+            return;
+        } else {
+            recordSpecificGoalDays++;
+            recordSpecificGoalDaysFlag = true;
+        }
+    }
+
+    //친구 신청을 한 경우
+    public void addFriendRequestCnt() {
+        this.friendRequestCnt += 1;
+    }
+
+    //친구 글에서 댓글을 단 경우
+    public void addCommentCntInFriendDay() {
+        this.commentCntInFriendDay += 1;
+        addComment();
+    }
+
+    //응원해요 누른 경우
+    public void addLikeCnt() {
+        this.likeCnt += 1;
+    }
+
+    //분발해요 누른 경우
+    public void addEncourageCnt() {
+        this.encourageCnt += 1;
+    }
+
+    //새로운 목표를 추가한 경우
+    public void addGoalRecordCnt() {
+        this.goalRecordCnt += 1;
+    }
+
+    //푸시 열람 수 추가
+    public void addPushOpenCnt() {
+        this.pushOpenCnt += 1;
+    }
+
+    //하루에 3개 이상의 목표를 완료한 경우: 7일 연속 목표 달성 로직도 체크
+    public void addComplete3Goal() {
+        this.complete3goal += 1;
+        setRecordSpecificGoalDays();
+    }
+
+    //하루에 3번 이상 친구 신청을 날린 경우
+    public void addRequestFriendOneDay() {
+        this.requestFriendOneDay += 1;
+    }
 }
