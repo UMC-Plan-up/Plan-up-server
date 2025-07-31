0e773fcd23cefc530017bd057e262d7eed9e6901
leesuyong849 <toosee1005@cau.ac.kr>
2025-07-31 15:34:39 +0900

    FIX: error 해결 중

--- src/main/java/com/planup/planup/domain/goal/repository/PhotoVerificationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.planup.planup.domain.goal.repository;
-
-import com.planup.planup.domain.goal.entity.PhotoVerification;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PhotoVerificationRepository extends JpaRepository<PhotoVerification, Long> {
-}
--- src/main/java/com/planup/planup/domain/goal/repository/TimerVerificationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.planup.planup.domain.goal.repository;
-
-import com.planup.planup.domain.goal.entity.TimerVerification;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TimerVerificationRepository extends JpaRepository<TimerVerification, Long> {
-}
