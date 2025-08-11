0de7521ec4fe1338efee13c236cb570265ee5c44
OrangeKim04 <queemkimu@naver.com>
2025-08-11 15:07:36 +0900

    feat: 비밀번호 변경 이메일 발송 요청 DTO 생성

---
+++ src/main/java/com/planup/planup/domain/user/dto/PasswordChangeEmailRequestDTO.java
@@ -0,0 +1,21 @@
+package com.planup.planup.domain.user.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PasswordChangeEmailRequestDTO {
+
+    @Schema(description = "이메일", example = "user@planup.com")
+    @NotBlank(message = "이메일은 필수입니다")
+    @Email(message = "이메일 형식을 확인해주세요")
+    private String email;
+}
