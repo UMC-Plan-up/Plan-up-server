8812487c4663cc3a037193314dacadecf2128430
leesuyong849 <toosee1005@cau.ac.kr>
2025-08-12 11:48:42 +0900

    FEAT: GoalExpireScheduler 설정 완료

--- src/main/java/com/planup/planup/domain/goal/repository/GoalRepository.java
+++ src/main/java/com/planup/planup/domain/goal/repository/GoalRepository.java
@@ -4,6 +4,11 @@ import com.planup.planup.domain.goal.entity.Goal;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+import java.util.List;
+
 @Repository
 public interface GoalRepository extends JpaRepository<Goal, Long> {
+
+    List<Goal> findByEndDateBeforeAndIsActiveTrue(Date now);
 }
