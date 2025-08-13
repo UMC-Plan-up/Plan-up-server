ad39e3545d7d882436687c17b3924782d3cfbab8
leesuyong849 <toosee1005@cau.ac.kr>
2025-08-13 15:45:12 +0900

    FEAT: 시간이 지난 goal에 대해 만약 challenge라면 종료 메세지 보내는 로직 추가

--- src/main/java/com/planup/planup/domain/goal/service/GoalLifeService.java
+++ src/main/java/com/planup/planup/domain/goal/service/GoalLifeService.java
@@ -1,7 +1,9 @@
 package com.planup.planup.domain.goal.service;
 
+import com.planup.planup.domain.goal.entity.Challenge;
 import com.planup.planup.domain.goal.entity.Goal;
 import com.planup.planup.domain.goal.repository.GoalRepository;
+import com.planup.planup.domain.notification.service.NotificationCreateService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,11 +16,19 @@ import java.util.List;
 public class GoalLifeService {
 
     private final GoalRepository goalRepository;
+    private final ChallengeService challengeService;
+    private final NotificationCreateService notificationCreateService;
 
     @Transactional
     public void disableExpiredGoals(Date date) {
         List<Goal> goal = goalRepository.findByEndDateBeforeAndIsActiveTrue(date);
         goal.forEach(Goal::setInActive);
+
+        //만약 챌린지라면
+        goal.stream()
+                .filter(Goal::isChallenge)
+                .map(g -> (Challenge) g)
+                .forEach(notificationCreateService::createChallengeNotification);
     }
 }
 
