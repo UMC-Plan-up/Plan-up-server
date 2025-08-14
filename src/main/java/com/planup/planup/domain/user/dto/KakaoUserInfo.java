e87c0e08a5ab5bd523a59b6c141bd4da4928ad29
Kwon-DoHee <152317074+seamooll@users.noreply.github.com>
2025-08-14 13:32:38 +0900

    Fix: KakaoUserInfo 중복 어노테이션 문제 수정

--- src/main/java/com/planup/planup/domain/user/dto/KakaoUserInfo.java
+++ src/main/java/com/planup/planup/domain/user/dto/KakaoUserInfo.java
@@ -5,9 +5,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
-
-@Getter
-@Setter
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class KakaoUserInfo {
 
