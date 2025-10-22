8bdb4dcb3b705398817d2c46c226ced2f02739e3
leesuyong849 <toosee1005@cau.ac.kr>
2025-10-22 20:41:19 +0900

    FEAT: EnableAsync 어노테이션 추가

--- src/main/java/com/planup/planup/PlanupApplication.java
+++ src/main/java/com/planup/planup/PlanupApplication.java
@@ -3,11 +3,13 @@ package com.planup.planup;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableScheduling
+@EnableAsync
 public class PlanupApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PlanupApplication.class, args);
