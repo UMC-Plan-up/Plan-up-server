fb8c5b5594fd0ae5066da8232fc5b31279c7867f
leesuyong849 <toosee1005@cau.ac.kr>
2025-12-07 18:34:21 +0900

    FEAT: repository, service 계층 추가

---
+++ src/main/java/com/planup/planup/domain/bedge/repository/SpecificGoalDaysRepository.java
@@ -0,0 +1,7 @@
+package com.planup.planup.domain.bedge.repository;
+
+import com.planup.planup.domain.bedge.entity.SpecificGoalDays;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SpecificGoalDaysRepository extends JpaRepository<SpecificGoalDays, Long> {
+}
