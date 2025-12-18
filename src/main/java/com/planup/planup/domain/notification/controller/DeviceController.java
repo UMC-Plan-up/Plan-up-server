8bbb75833c63e3d0f582a927cb4c75a072fd8d0f
leesuyong849 <toosee1005@cau.ac.kr>
2025-12-18 23:21:36 +0900

    FEAT: deactive 하는 코드 추가

--- src/main/java/com/planup/planup/domain/notification/controller/DeviceController.java
+++ src/main/java/com/planup/planup/domain/notification/controller/DeviceController.java
@@ -1,14 +1,12 @@
 package com.planup.planup.domain.notification.controller;
 
+import com.google.protobuf.Api;
 import com.planup.planup.apiPayload.ApiResponse;
 import com.planup.planup.domain.notification.dto.DeviceTokenRequestDTO;
 import com.planup.planup.domain.notification.service.deviceTokenService.DeviceTokenService;
 import com.planup.planup.validation.annotation.CurrentUser;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,6 +18,18 @@ public class DeviceController {
     @PostMapping("post")
     public ApiResponse<Boolean> postDeviceTokenFromUser(@RequestBody DeviceTokenRequestDTO dto, @CurrentUser Long userId) {
         deviceTokenService.upsert(userId, dto.token(), dto.plateFrom(), dto.appVersion(), dto.local());
+        return ApiResponse.onSuccess(true);
+    }
+
+    @PatchMapping("/deactivate/token/{token}")
+    public ApiResponse<Boolean> deactivateByToken(@PathVariable String token) {
+        deviceTokenService.deactivateByToken(token);
+        return ApiResponse.onSuccess(true);
+    }
+
+    @PatchMapping("/deactivate/userId")
+    public ApiResponse<Boolean> deactivateByUserId(@CurrentUser Long userId) {
+        deviceTokenService.deactivateAllByUser(userId);
         return ApiResponse.onSuccess(true);
     }
 }
