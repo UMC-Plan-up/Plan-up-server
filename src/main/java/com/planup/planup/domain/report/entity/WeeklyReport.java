c56b34be8da1907faff457c4263d7d6bdbb4620d
leesuyong849 <toosee1005@cau.ac.kr>
2026-01-21 11:32:55 +0900

    REF: weeklyReport 제약 추가

--- src/main/java/com/planup/planup/domain/report/entity/WeeklyReport.java
+++ src/main/java/com/planup/planup/domain/report/entity/WeeklyReport.java
@@ -17,6 +17,15 @@ import java.util.List;
 @AllArgsConstructor
 @ToString(exclude = {"user", "goalReports", "dailyRecords"})
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Table(
+        name = "weekly_report",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "uk_weekly_report_user_year_month_week",
+                        columnNames = {"user_id", "year", "month", "week_number"}
+                )
+        }
+)
 public class WeeklyReport extends BaseTimeEntity {
 
     @Id
@@ -30,6 +39,7 @@ public class WeeklyReport extends BaseTimeEntity {
 
     private int year;
     private int month;
+    @Column(name = "week_number")
     private int weekNumber;
 
     private LocalDateTime startDate;
