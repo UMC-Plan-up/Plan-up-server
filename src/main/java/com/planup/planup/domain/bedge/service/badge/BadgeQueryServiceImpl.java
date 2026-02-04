4a46b76e6c071f53d3758c3145553eb470a17fe2
leesuyong849 <toosee1005@cau.ac.kr>
2026-02-04 23:51:39 +0900

    FEAT: getUserBadgeList 추가

--- src/main/java/com/planup/planup/domain/bedge/service/badge/BadgeQueryServiceImpl.java
+++ src/main/java/com/planup/planup/domain/bedge/service/badge/BadgeQueryServiceImpl.java
@@ -1,9 +1,18 @@
 package com.planup.planup.domain.bedge.service.badge;
 
+import com.planup.planup.domain.bedge.converter.BadgeConverter;
+import com.planup.planup.domain.bedge.dto.BadgeResponseDTO;
+import com.planup.planup.domain.user.entity.User;
+import com.planup.planup.domain.user.entity.UserBadge;
 import com.planup.planup.domain.user.repository.UserBadgeRepository;
+import com.planup.planup.domain.user.repository.UserRepository;
+import com.planup.planup.domain.user.service.query.UserQueryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -11,6 +20,11 @@ import org.springframework.transaction.annotation.Transactional;
 public class BadgeQueryServiceImpl implements BadgeQueryService {
 
     private final UserBadgeRepository userBadgeRepository;
+    private final UserQueryService userQueryService;
 
-
+    public List<BadgeResponseDTO.SimpleBadgeDTO> getUserBadgeList(Long userId) {
+        User user = userQueryService.getUserByUserId(userId);
+        List<UserBadge> userBadges = userBadgeRepository.findAllByUser(user);
+        return userBadges.stream().map(ub -> BadgeConverter.toSimpleBadgeDTO(ub.getBadgeType())).collect(Collectors.toList());
+    }
 }
