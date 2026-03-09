346d724ae4ce261166de7573ededd8e70d18c144
leesuyong849 <toosee1005@cau.ac.kr>
2026-03-09 12:32:50 +0900

    FIX: userTermsConvertor 컴포넌트 등록

--- src/main/java/com/planup/planup/domain/user/converter/UserTermsConvertor.java
+++ src/main/java/com/planup/planup/domain/user/converter/UserTermsConvertor.java
@@ -4,9 +4,11 @@ import com.planup.planup.domain.user.dto.AuthRequestDTO;
 import com.planup.planup.domain.user.entity.Terms;
 import com.planup.planup.domain.user.entity.User;
 import com.planup.planup.domain.user.entity.UserTerms;
+import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 
+@Component
 public class UserTermsConvertor {
     // ======= 약관동의 =======
     public UserTerms toUserTermsEntity(User user, Terms terms, AuthRequestDTO.TermsAgreement agreement) {
