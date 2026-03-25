7312277fed1f5763fae1b45500d4e888a6d16e6b
leesuyong849 <toosee1005@cau.ac.kr>
2026-03-25 17:47:49 +0900

    FEAT: 사용하지 않는 goalpolicy 주석처리

--- src/main/java/com/planup/planup/domain/goal/service/policy/GoalPolicy.java
+++ src/main/java/com/planup/planup/domain/goal/service/policy/GoalPolicy.java
@@ -13,12 +13,12 @@ public class GoalPolicy {
     private final FriendReadService friendReadService;
     private final UserGoalService userGoalService;
 
-    public void ensureSendInviteFriendTpGoal(Long userId, Long friendId, Long goalId) {
-        //친구인지
-        friendReadService.isFriend(userId, friendId);
-
-        //이미 해당 Goal에 참여중인지
-        boolean exist = userGoalService.existUserGoal(goalId, userId);
-        if ()
-    }
+//    public void ensureSendInviteFriendTpGoal(Long userId, Long friendId, Long goalId) {
+//        //친구인지
+//        friendReadService.isFriend(userId, friendId);
+//
+//        //이미 해당 Goal에 참여중인지
+//        boolean exist = userGoalService.existUserGoal(goalId, userId);
+//        if ()
+//    }
 }
